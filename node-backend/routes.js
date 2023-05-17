2ac6d528e03ecc15d120462f70e2fc514240e6f2
Matteo <matteo.barone3@gmail.com>
2023-05-17 09:56:20 +0200

    Login api working

--- node-backend/routes.js
+++ node-backend/routes.js
@@ -7,31 +7,37 @@ router.use(function (req,res,next){
     res.header("Access-Control-Allow-Headers","auth-token, Origin, X-Requested-With, Content-Type, Accept");
     next();
 })
-router.get('/',async (req,res) => {
+router.get('/login',async (req,res) => {
     const {email,password} = req.query;
     if(email === undefined || password === undefined){
         res.status(400);
-        res.send("Credenziali mancanti");
-        return;
+        return res.send("Missing credential");
+
     }
     try{
-        await User.findOne({email,password});
-        //qui non funziona, mettere un throw?
-        res.send("Credenziali corrette");
-        return;
-    }catch (error){
-        res.status(401);
-        res.send("Credenziali errate");
-        return;
-    }
+        const result = await User.findOne({email,password});
+        if(result === null){
+            res.status(401);
+            return res.send("Wrong credentials");
+        }else{
+            return res.send("Correct credential");
 
+        }
+    }catch (error){
+        res.status(500);
+        return res.send("Internal Error");
+    }
 })
 //da testare
 router.post('/users', async (req, res) => {
     const { username, password, nomeAzienda, partitaIVA } = req.body;
     const newUser = new User({ username, password,nomeAzienda,partitaIVA });
     const response  = newUser.save();
-
+    if(response){
+        return res.status(500).send("Internal error");
+    }else{
+        return res.send("User successfully registered");
+    }
 
 });
 
