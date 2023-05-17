b32dd61d7b8ead3ef369fba66b8f9172f5077cd4
Matteo <matteo.barone3@gmail.com>
2023-05-17 11:02:18 +0200

    Register user done

--- node-backend/routes.js
+++ node-backend/routes.js
@@ -1,17 +1,19 @@
 import express from "express";
 import {User} from "./database.js"
 const router = express.Router();
+
 router.use(function (req,res,next){
     res.header("Access-Control-Allow-Origin","*");
     res.header("Access-Control-Allow-Methods","GET,HEAD,OPTION,POST,PUT,DELETE");
     res.header("Access-Control-Allow-Headers","auth-token, Origin, X-Requested-With, Content-Type, Accept");
     next();
 })
+
 router.get('/login',async (req,res) => {
     const {email,password} = req.query;
     if(email === undefined || password === undefined){
         res.status(400);
-        return res.send("Missing credential");
+        return res.send("Missing credentials");
 
     }
     try{
@@ -20,25 +22,47 @@ router.get('/login',async (req,res) => {
             res.status(401);
             return res.send("Wrong credentials");
         }else{
-            return res.send("Correct credential");
+            return res.send("Correct credentials");
 
         }
     }catch (error){
         res.status(500);
-        return res.send("Internal Error");
+        return res.send("Internal Server Error");
     }
 })
-//da testare
-router.post('/users', async (req, res) => {
-    const { username, password, nomeAzienda, partitaIVA } = req.body;
-    const newUser = new User({ username, password,nomeAzienda,partitaIVA });
-    const response  = newUser.save();
-    if(response){
-        return res.status(500).send("Internal error");
-    }else{
-        return res.send("User successfully registered");
-    }
 
+router.post('/register', async (req, res) => {
+    try{
+        const {email, password, nomeAzienda, partitaIVA} = req.body;
+        const user = await User.findOne({email,password});
+        if(user !== null){
+            return res.status(400).send("User already exist, use another email");
+        }else{
+            const newUser = new User({email,password,nomeAzienda,partitaIVA});
+            const response  = newUser.save();
+            return res.status(200).send("User registered correctly");
+        }
+    }catch (error){
+        return res.status(500).send("Internal Server Error");
+    }
 });
+/*
+
+router.put('/changePassword',async (req,res) => {
+    const {email, password, newPassword} = req.body;
+    try{
+        const result = await User.findOne({email,password});
+
+    }catch(error){
+        return res.status(500).send("Internal Server Error");
+    }
+})
+
+//aggiungere cambio nomeAzienda e partitaIVA?
+
+router.delete('/removeUser',async (req,res) => {
+
+})
+ */
 
 export default router;
--- node-backend/server.js
+++ node-backend/server.js
@@ -6,8 +6,8 @@ connectDB();
 const PORT = process.env.PORT;
 const app = express();
 
-app.use('/', routes);
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use('/', routes);
+
 
 app.listen(PORT)
